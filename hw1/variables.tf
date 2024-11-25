919bd77ec7cb2621a16a4f05be4ddfd6a3caf050
alf3ratz <apetropavlovskij@yandex.ru>
2024-11-25 06:08:10 +0300

    hw1

---
+++ hw1/variables.tf
@@ -0,0 +1,12 @@
+variable "yc_token" {
+    default="y0_AgAAAAAiHjY3AATuwQAAAAEZ2Mh6AAD7BmEyquNGHrTGvCVmhfGflpFYbQ"
+}
+variable "yc_cloud_id" {
+    default="b1g9gvqep08q3gcm3qd1"
+}
+variable "yc_folder_id" {
+    default="b1gs1mnpsgc0ud5le3h4"
+}
+variable "yc_zone" {
+    default="ru-central1-a"
+}
